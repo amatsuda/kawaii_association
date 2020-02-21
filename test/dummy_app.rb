be6e8abc1a223034e54031adc0efa7724f7ea2c5
Akira Matsuda <ronnie@dio.jp>
2020-02-21 20:22:04 +0900

    Prepare for tests

---
+++ test/dummy_app.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# config
+ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
+
+module KawaiiAssociationTestApp
+  Application = Class.new(Rails::Application) do
+    config.eager_load = false
+    config.active_support.deprecation = :log
+  end.initialize!
+end
+
+# models
+class User < ActiveRecord::Base; end
+
+# migrations
+class CreateAllTables < ActiveRecord::VERSION::MAJOR >= 5 ? ActiveRecord::Migration[5.0] : ActiveRecord::Migration
+  def self.up
+    create_table(:users) {|t| t.string :name; t.string :email; t.integer :age }
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,19 @@
+# frozen_string_literal: true
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+# load Rails first
+require 'rails'
+require 'active_record'
+require 'byebug'
+
+# load the plugin
 require "kawaii_association"
 
-require "minitest/autorun"
+# needs to load the app next
+require 'dummy_app'
+
+require 'test/unit/rails/test_help'
+
+CreateAllTables.up unless ActiveRecord::Base.connection.table_exists? 'posts'
